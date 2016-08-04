ccfb53aae4f62b3cd39b9b0b720965cbba679fb7
Wakaba <wakaba@suikawiki.org>
2016-08-04 15:40:05 +0900

    Don't use Inline

--- t_deps/lib/Test/OpenSSL.pm
+++ t_deps/lib/Test/OpenSSL.pm
@@ -1,22 +1,11 @@
 package Test::OpenSSL;
 use strict;
 use warnings;
-use DynaLoader;
-use Path::Tiny;
-use Inline 'C', 'config', inc => '-I' . path (__FILE__)->parent->parent->parent->parent->child ('local/common/include')->absolute->stringify;
-use Inline 'C';
 
-our $ref = DynaLoader::dl_load_file ("libssl.so", 0x01);
+# #define SSL_set_tlsext_status_ocsp_resp(ssl, arg, arglen) \
+# SSL_ctrl(ssl,SSL_CTRL_SET_TLSEXT_STATUS_REQ_OCSP_RESP,arglen, (void *)arg)
+sub p_SSL_set_tlsext_status_ocsp_resp_data ($$$) {
+  return Net::SSLeay::ctrl ($_[0], 71, $_[2], $_[1]);
+}
 
 1;
-
-__DATA__
-__C__
-#include <openssl/ssl.h>
-#include <openssl/tls1.h>
-#include <openssl/ocsp.h>
-#include <openssl/XXX.h>
-
-int p_SSL_set_tlsext_status_ocsp_resp_data (int ssl, unsigned char *res, int len) {
-  return SSL_set_tlsext_status_ocsp_resp ((void *) ssl, res, len);
-}

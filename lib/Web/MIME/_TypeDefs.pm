b21610ef1dc9bdfe21297541b205e66d0e054ee9
gaa <gaa@gaa3.h>
2016-08-04 22:11:21 +0000

    auto

--- lib/Web/MIME/_TypeDefs.pm
+++ lib/Web/MIME/_TypeDefs.pm
@@ -10710,16 +10710,7 @@ $Web::MIME::_TypeDefs::Type = {
                                                                                  'application/x-wordperfect6.0' => {}
                                                                                }
                                                                 },
-                                            'wordperfect6.0' => {
-                                                                  'related' => {
-                                                                                 'application/vnd.wordperfect' => {},
-                                                                                 'application/vnd.wordperfect5.1' => {},
-                                                                                 'application/wordperfect' => {},
-                                                                                 'application/wordperfect5.1' => {},
-                                                                                 'application/x-wordperfect' => {},
-                                                                                 'application/x-wordperfect6.0' => {}
-                                                                               }
-                                                                },
+                                            'wordperfect6.0' => {},
                                             'wordperfect6.1' => {
                                                                   'obsolete' => 1,
                                                                   'preferred_type' => 'application/x-wordperfect6.1',

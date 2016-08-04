04cf3e7955208e0e9912f9efebb0f72127866a95
Wakaba <wakaba@suikawiki.org>
2016-08-04 14:52:22 +0900

    debug

--- t_deps/lib/Test/OpenSSL.pm
+++ t_deps/lib/Test/OpenSSL.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use DynaLoader;
 use Path::Tiny;
-#use Inline 'C', 'config', inc => '-I' . path (__FILE__)->parent->parent->parent->parent->child ('local/common/include')->absolute->stringify;
+use Inline 'C', 'config', inc => '-I' . path (__FILE__)->parent->parent->parent->parent->child ('local/common/include')->absolute->stringify;
 use Inline 'C';
 
 our $ref = DynaLoader::dl_load_file ("libssl.so", 0x01);
@@ -18,7 +18,5 @@ __C__
 #include <openssl/XXX.h>
 
 int p_SSL_set_tlsext_status_ocsp_resp_data (int ssl, unsigned char *res, int len) {
-  unsigned char *p = OPENSSL_malloc (len);
-  memcpy (p, res, len);
-  return SSL_set_tlsext_status_ocsp_resp ((void *) ssl, p, len);
+  return SSL_set_tlsext_status_ocsp_resp ((void *) ssl, res, len);
 }

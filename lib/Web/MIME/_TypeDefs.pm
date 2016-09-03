926e625a28cb212a86077dbea570391c3e82c966
gaa <gaa@gaa3.h>
2016-09-03 03:32:48 +0000

    auto

--- lib/Web/MIME/_TypeDefs.pm
+++ lib/Web/MIME/_TypeDefs.pm
@@ -3880,7 +3880,11 @@ $Web::MIME::_TypeDefs::Type = {
                                             'testfontstream' => {},
                                             'tex' => {},
                                             'texinfo' => {},
-                                            'text' => {},
+                                            'text' => {
+                                                        'related' => {
+                                                                       'application/octet-stream' => {}
+                                                                     }
+                                                      },
                                             'tga' => {
                                                        'related' => {
                                                                       'image/x-targa' => {}
@@ -10721,16 +10725,7 @@ $Web::MIME::_TypeDefs::Type = {
                                                                                  'application/x-wordperfect6.0' => {}
                                                                                }
                                                                 },
-                                            'wordperfect6.0' => {
-                                                                  'related' => {
-                                                                                 'application/vnd.wordperfect' => {},
-                                                                                 'application/vnd.wordperfect5.1' => {},
-                                                                                 'application/wordperfect' => {},
-                                                                                 'application/wordperfect5.1' => {},
-                                                                                 'application/x-wordperfect' => {},
-                                                                                 'application/x-wordperfect6.0' => {}
-                                                                               }
-                                                                },
+                                            'wordperfect6.0' => {},
                                             'wordperfect6.1' => {
                                                                   'obsolete' => 1,
                                                                   'preferred_type' => 'application/x-wordperfect6.1',

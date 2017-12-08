ce784882217303d43d9391283c8b84d1fd22c488
gaa <gaa@gaa3.h>
2017-12-08 06:33:37 +0000

    auto

--- lib/Web/MIME/_TypeDefs.pm
+++ lib/Web/MIME/_TypeDefs.pm
@@ -652,7 +652,6 @@ $Web::MIME::_TypeDefs::Type = {
                                                          'iana' => 'permanent',
                                                          'iana_intended_usage' => 'common'
                                                        },
-                                          'font-woff2' => {},
                                           'force-download' => {},
                                           'fractals' => {},
                                           'frame-idraw' => {},
@@ -4859,9 +4858,6 @@ $Web::MIME::_TypeDefs::Type = {
                                                                                                          'iana_intended_usage' => 'common'
                                                                                                        },
                                           'vnd.openxmlformats-officedocument.pres' => {},
-                                          'vnd.openxmlformats-officedocument.presentationml-template' => {
-                                                                                                         'iana_intended_usage' => 'common'
-                                                                                                       },
                                           'vnd.openxmlformats-officedocument.presentationml.commentauthors+xml' => {
                                                                                                                    'iana' => 'permanent',
                                                                                                                    'iana_intended_usage' => 'common'
@@ -4963,9 +4959,6 @@ $Web::MIME::_TypeDefs::Type = {
                                                                                                               'iana' => 'permanent',
                                                                                                               'iana_intended_usage' => 'common'
                                                                                                             },
-                                          'vnd.openxmlformats-officedocument.spreadsheetml-template' => {
-                                                                                                        'iana_intended_usage' => 'common'
-                                                                                                      },
                                           'vnd.openxmlformats-officedocument.spreadsheetml.calcchain+xml' => {
                                                                                                              'iana' => 'permanent',
                                                                                                              'iana_intended_usage' => 'common'
@@ -5089,9 +5082,6 @@ $Web::MIME::_TypeDefs::Type = {
                                                                                             'iana_intended_usage' => 'limited use'
                                                                                           },
                                           'vnd.openxmlformats-officedocument.word' => {},
-                                          'vnd.openxmlformats-officedocument.wordprocessingml-template' => {
-                                                                                                           'iana_intended_usage' => 'common'
-                                                                                                         },
                                           'vnd.openxmlformats-officedocument.wordprocessingml.comments+xml' => {
                                                                                                                'iana' => 'permanent',
                                                                                                                'iana_intended_usage' => 'common'
@@ -12875,12 +12865,12 @@ $Web::MIME::_TypeDefs::Sniffing = {
                          'application/x-rar-compressed'
                        ],
                        [
-                         qr/(?:PK\x03\x04)/,
-                         'application/zip'
-                       ],
-                       [
                          qr/(?:\x1F\x8B\x08)/,
                          'application/x-gzip'
+                       ],
+                       [
+                         qr/(?:PK\x03\x04)/,
+                         'application/zip'
                        ]
                      ],
           'audio_or_video' => [
@@ -12889,28 +12879,28 @@ $Web::MIME::_TypeDefs::Sniffing = {
                                 'audio/midi'
                               ],
                               [
-                                qr/(?:RIFF[\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF]AVI\x20)/,
-                                'video/avi'
-                              ],
-                              [
                                 qr/(?:\.snd)/,
                                 'audio/basic'
-                              ],
-                              [
-                                qr/(?:RIFF[\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF]WAVE)/,
-                                'audio/wave'
                               ],
                               [
                                 qr/(?:FORM[\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF]AIFF)/,
                                 'audio/aiff'
                               ],
                               [
+                                qr/(?:OggS\x00)/,
+                                'application/ogg'
+                              ],
+                              [
+                                qr/(?:RIFF[\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF]AVI\x20)/,
+                                'video/avi'
+                              ],
+                              [
                                 qr/(?:ID3)/,
                                 'audio/mpeg'
                               ],
                               [
-                                qr/(?:OggS\x00)/,
-                                'application/ogg'
+                                qr/(?:RIFF[\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF]WAVE)/,
+                                'audio/wave'
                               ]
                             ],
           'bom1' => [
@@ -12927,12 +12917,16 @@ $Web::MIME::_TypeDefs::Sniffing = {
                   ],
           'font' => [
                     [
-                      qr/(?:ttcf)/,
-                      'font/collection'
+                      qr/(?:\x00\x01\x00\x00)/,
+                      'font/ttf'
                     ],
                     [
                       qr/(?:[\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF]LP)/,
                       'application/vnd.ms-fontobject'
+                    ],
+                    [
+                      qr/(?:ttcf)/,
+                      'font/collection'
                     ],
                     [
                       qr/(?:wOFF)/,
@@ -12941,36 +12935,32 @@ $Web::MIME::_TypeDefs::Sniffing = {
                     [
                       qr/(?:OTTO)/,
                       'font/otf'
-                    ],
-                    [
-                      qr/(?:\x00\x01\x00\x00)/,
-                      'font/ttf'
                     ]
                   ],
           'image' => [
                      [
-                       qr/(?:RIFF[\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF]WEBPVP)/,
-                       'image/webp'
-                     ],
-                     [
-                       qr/(?:BM)/,
-                       'image/bmp'
-                     ],
-                     [
                        qr/(?:\x00\x00[\x01\x02]\x00)/,
                        'image/x-icon'
-                     ],
-                     [
-                       qr/(?:\xFF\xD8\xFF)/,
-                       'image/jpeg'
                      ],
                      [
                        qr/(?:GIF8[79]a)/,
                        'image/gif'
                      ],
                      [
+                       qr/(?:BM)/,
+                       'image/bmp'
+                     ],
+                     [
+                       qr/(?:RIFF[\x00-\xFF][\x00-\xFF][\x00-\xFF][\x00-\xFF]WEBPVP)/,
+                       'image/webp'
+                     ],
+                     [
                        qr/(?:\x89PNG\x0D\x0A\x1A\x0A)/,
                        'image/png'
+                     ],
+                     [
+                       qr/(?:\xFF\xD8\xFF)/,
+                       'image/jpeg'
                      ]
                    ],
           'non_scriptable' => [
@@ -12981,16 +12971,16 @@ $Web::MIME::_TypeDefs::Sniffing = {
                             ],
           'scriptable' => [
                           [
-                            qr/(?:%PDF-)/,
-                            'application/pdf'
+                            qr/(?:[\x09\x0A\x0C\x0D\x20]*<\?xml)/,
+                            'text/xml'
                           ],
                           [
                             qr/(?:(?:[\x09\x0A\x0C\x0D\x20]*<(?:[Ss](?:[Cc][Rr][Ii][Pp][Tt][\x20>]|[Tt][Yy][Ll][Ee][\x20>])|[Tt](?:[Aa][Bb][Ll][Ee][\x20>]|[Ii][Tt][Ll][Ee][\x20>])|[Hh](?:[Ee][Aa][Dd][\x20>]|[Tt][Mm][Ll][\x20>]|1[\x20>])|[Bb](?:[\x20>]|[Oo][Dd][Yy][\x20>]|[Rr][\x20>])|[Ii][Ff][Rr][Aa][Mm][Ee][\x20>]|[Ff][Oo][Nn][Tt][\x20>]|[Dd][Ii][Vv][\x20>]|[Aa][\x20>]|[Pp][\x20>]|!--)|<![Dd][Oo][Cc][Tt][Yy][Pp][Ee]\x20[Hh][Tt][Mm][Ll][\x20>]))/,
                             'text/html'
                           ],
                           [
-                            qr/(?:[\x09\x0A\x0C\x0D\x20]*<\?xml)/,
-                            'text/xml'
+                            qr/(?:%PDF-)/,
+                            'application/pdf'
                           ]
                         ],
           'text_track' => [

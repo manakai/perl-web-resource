094962dbb9421e47ecfc5f61a5fa92af63ba598a
Wakaba <wakaba@suikawiki.org>
2024-04-02 20:08:32 +0900

    Magic for Perl version dependent XS brokenness

--- t_deps/lib/Test/OpenSSL.pm
+++ t_deps/lib/Test/OpenSSL.pm
@@ -5,7 +5,8 @@ use warnings;
 # #define SSL_set_tlsext_status_ocsp_resp(ssl, arg, arglen) \
 # SSL_ctrl(ssl,SSL_CTRL_SET_TLSEXT_STATUS_REQ_OCSP_RESP,arglen, (void *)arg)
 sub p_SSL_set_tlsext_status_ocsp_resp_data ($$$) {
-  return Net::SSLeay::ctrl ($_[0], 71, $_[2], ''.$_[1]);
+  my $x = '' . $_[1];
+  return Net::SSLeay::ctrl ($_[0], 71, $_[2], $x);
 }
 
 1;

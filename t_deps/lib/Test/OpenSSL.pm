3983daeb76593b5072adee00a8f7c98e86a71bf8
Wakaba <wakaba@suikawiki.org>
2026-09-14 19:34:40 +0900

    Fixed handling of broken OCSP data

--- t_deps/lib/Test/OpenSSL.pm
+++ t_deps/lib/Test/OpenSSL.pm
@@ -2,11 +2,12 @@ package Test::OpenSSL;
 use strict;
 use warnings;
 
-# #define SSL_set_tlsext_status_ocsp_resp(ssl, arg, arglen) \
-# SSL_ctrl(ssl,SSL_CTRL_SET_TLSEXT_STATUS_REQ_OCSP_RESP,arglen, (void *)arg)
+## Added by Net::SSLeay 1.82 (31 October Heisei 29)
 sub p_SSL_set_tlsext_status_ocsp_resp_data ($$$) {
   my $x = '' . $_[1];
-  return Net::SSLeay::ctrl ($_[0], 71, $_[2], $x);
+  return Net::SSLeay::set_tlsext_status_ocsp_resp ($_[0], $x);
 }
 
 1;
+
+## License: Public Domain.
